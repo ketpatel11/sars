e4e0e29bfc9bc6fc69410c9e49ba2236fa4735d9
Paulo Kuong <paulo@theorchard.com>
2015-06-11 14:51:35 -0400

    Make makeRequest function

--- app/src/main/java/com/theorchard/sample/sample/MainActivity.java
+++ app/src/main/java/com/theorchard/sample/sample/MainActivity.java
@@ -2,6 +2,7 @@ package com.theorchard.sample.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,50 +10,79 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.JSONObject;
+
 
 public class MainActivity extends Activity {
     private ListView l;
     private CheckBox check1;
     private Button button1;
+    static InputStream is = null;
+    static JSONObject jObj = null;
+    static String json = "";
 
-    String[] days={"Monday","Tuesday"};
+    String[] days = {"", ""};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addListenerOnButton();
         l = (ListView) findViewById(R.id.listView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,days);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, days);
         l.setAdapter(adapter);
+
+
     }
 
+    private String makeRequest(String url) {
+        String outdata = "";
+        try {
+            URL url_g = new URL(url);
+            URLConnection ukr = url_g.openConnection();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(ukr.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                outdata += inputLine;
+            in.close();
+        } catch (java.io.IOException e) {
+            System.out.print(e.getMessage());
+        }
+        return outdata;
+    }
+
+
     public void addListenerOnButton() {
-        check1 = (CheckBox)findViewById(R.id.checkBox);
-        button1 = (Button)findViewById(R.id.button);
+        button1 = (Button) findViewById(R.id.button);
         button1.setOnClickListener(
-                new View.OnClickListener(){
+                new View.OnClickListener() {
                     @Override
-                    public void onClick(View v){
-                        StringBuffer result = new StringBuffer();
+                    public void onClick(View v) {
+                        /*StringBuffer result = new StringBuffer();
                         result.append("Dog: ").append(check1.isChecked());
                         Toast.makeText(MainActivity.this, result.toString(),
-                                Toast.LENGTH_LONG).show();
+                                Toast.LENGTH_LONG).show();*/
+                        TextView tv = (TextView) findViewById(R.id.textView);
+                        String responseText = makeRequest("http://192.168.31.128/streams/600353005129/5");
+                        tv.setText(responseText);
                     }
                 }
         );
+
+
     }
 
     @Override
@@ -77,54 +107,5 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public String requestContent(String url) {
-        HttpClient httpclient = new DefaultHttpClient();
-        String result = null;
-        HttpGet httpget = new HttpGet(url);
-        HttpResponse response = null;
-        InputStream instream = null;
 
-        try {
-            response = httpclient.execute(httpget);
-            HttpEntity entity = response.getEntity();
-
-            if (entity != null) {
-                instream = entity.getContent();
-                result = convertStreamToString(instream);
-            }
-
-        } catch (Exception e) {
-            // manage exceptions
-        } finally {
-            if (instream != null) {
-                try {
-                    instream.close();
-                } catch (Exception exc) {
-
-                }
-            }
-        }
-
-        return result;
-    }
-
-    public String convertStreamToString(InputStream is) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-            }
-        }
-
-        return sb.toString();
-    }
 }
